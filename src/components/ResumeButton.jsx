2c53580a09545d0f7847fdccccd97f7655abe98d
Dario <darioandrade@email.arizona.edu>
2021-05-07 10:11:59 -0700

    Updated Resume display
    Added UseEffect Hook to determine how to display resume based on media screen

--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PDFComponent from "./PDFComponent";
 
 const ResumeButton = (props) => {
   const { name, pdf } = props;
   const [active, setActive] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      setIsMobile(window.screen.width <= 768);
+    }
+    window.addEventListener("resize", handleResize);
+  });
+
   //let classes = "btn btn-lg btn-primary btn-";
   //active ? (classes += "outline-success") : (classes += "success");
 
@@ -15,9 +24,19 @@ const ResumeButton = (props) => {
   return (
     <React.Fragment>
       <div className="ResumeButtonBackground">
-        <button onClick={showResume} className="ResumeButton">
-          {name}
-        </button>
+        {!isMobile && (
+          <button onClick={showResume} className="ResumeButton">
+            {name}
+          </button>
+        )}
+        {isMobile && (
+          <a
+            target="_blank"
+            href="https://drive.google.com/file/d/1F3VYKopH97xEptrv6a4lniyct3KehkFP/view?usp=sharing"
+          >
+            <button className="ResumeButton">{name}</button>
+          </a>
+        )}
       </div>
       <div>{active && <PDFComponent file={pdf} active={active} />}</div>
     </React.Fragment>
